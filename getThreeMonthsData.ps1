20d1e84ea365a4904685afc26ddd281d3d756a8b
Nathan Schubkegel <nathan.schubkegel@gmail.com>
2019-11-18 22:26:38 -0800

    Initial contents - getCompanies.ps1 and getThreeMonthsData.ps1

---
+++ getThreeMonthsData.ps1
@@ -0,0 +1,55 @@
+param (
+  [switch]$forceDownload = $false,
+  [switch]$forceJsonToCsv = $false,
+  [string]$exchange = $(throw "-exchange is required"),
+  [string]$company = $(throw "-company is required")
+)
+
+$ErrorActionPreference = "Stop"
+$apiKey = Get-Content -Path "$PSScriptRoot\api_key.txt" -First 1
+
+$jsonPath = "$PSScriptRoot\data\$exchange\$company\threeMonths.json"
+$jsonAcquird = $false
+if ($forceDownload -or -not (Test-Path -Path $jsonPath -PathType Leaf)) {
+  $jsonAcquired = $true
+  Write-Host "Downloading data for company $company from exchange $exchange"
+  $unused = [system.io.directory]::CreateDirectory("$PSScriptRoot\data\$exchange\$company")
+  $from = (Get-Date).Subtract([System.TimeSpan]::FromDays(90)).ToString("yyyy-MM-dd")
+  $to = (Get-Date).ToString("yyyy-MM-dd")
+  $url = "https://www.quandl.com/api/v3/datasets/$exchange/$company.json?start_date=$from&end_date=$to&api_key=$apiKey"
+
+  [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls11 -bor [System.Net.SecurityProtocolType]::Tls12;
+  $wc = New-Object System.Net.WebClient
+  $unused = $wc.DownloadFile($url, $jsonPath)
+}
+
+$csvPath = "$PSScriptRoot\data\$exchange\$company\threeMonths.csv"
+if ($jsonAcquired -or $forceJsonToCsv -or -not (Test-Path -Path $csvPath -PathType Leaf)) {
+
+  function ArrayToCsv($fields) {
+    $result = ""
+    foreach ($field in $fields) {
+      if ($result -ne "") {
+        $result += ","
+      }
+      if ($field -ne $null) {
+        $result += '"' + $field.ToString().Replace('"', '""') + '"'
+      }
+      else {
+        $result += '""'
+      }
+    }
+    return $result
+  }
+
+  $json = Get-Content -Path $jsonPath | ConvertFrom-Json
+  $lines = @()
+  $lines += ArrayToCsv($json.dataset.column_names) 
+  foreach ($d in $json.dataset.data) {
+    $lines += ArrayToCsv($d)
+  }
+  $lines | Set-Content -Path $csvPath
+}
+
+$csv = Import-Csv -Path $csvPath
+$csv
